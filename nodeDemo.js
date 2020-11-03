2019e9f1ee508eef1fd548d7f075c4beabe1a7ec
XL2887 <67875303+XL2887@users.noreply.github.com>
2020-11-03 16:04:31 -0700

    Add files via upload

--- nodeDemo.js
+++ nodeDemo.js
@@ -1,28 +1,17 @@
-const http = require("https");
+const request = require('request');
 
 const options = {
-	"method": "GET",
-	"hostname": "rapidapi.p.rapidapi.com",
-	"port": null,
-	"path": "/statistics",
-	"headers": {
-		"x-rapidapi-key": "dcab7cf4a8mshebe0b1c9fd71d1bp1da46djsn8a2996cbb4d4",
-		"x-rapidapi-host": "covid-193.p.rapidapi.com",
-		"useQueryString": true
-	}
+  method: 'GET',
+  url: 'https://rapidapi.p.rapidapi.com/statistics',
+  headers: {
+    'x-rapidapi-key': 'dcab7cf4a8mshebe0b1c9fd71d1bp1da46djsn8a2996cbb4d4',
+    'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+    useQueryString: true
+  }
 };
 
-const req = http.request(options, function (res) {
-	const chunks = [];
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
+    console.log(body);
 });
-
-req.end();
